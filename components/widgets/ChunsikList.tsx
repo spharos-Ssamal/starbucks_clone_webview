5adc17c3cddf98eacb331789c97dd2d1a5218ae5
Jason Ahn <beat1103@gmail.com>
2023-02-28 17:04:01 +0900

    index

---
+++ components/widgets/ChunsikList.tsx
@@ -0,0 +1,25 @@
+function ChunsikList() {
+  return ( 
+    <section id="chunsik">
+      <div className="subtitle">
+        <h2>있다가 없어진 춘식이. 앱에서 사라졌어요</h2>
+      </div>
+      <div className="chunsik_items">
+        <div className="chunsik_item">
+          <div className="chunsik_item_img">
+            <img src="assets/images/item/goods/chunsik.png" alt="item_img" />
+          </div>
+          <div className="chunsik_item_name">
+            <p className="new">New</p>
+            <p>23 SS 체리 밸류 로맨틱 텀블러 335ml</p>
+          </div>
+          <div className="chunsik_item_cost">
+            <p><span>32,000</span>원</p>
+          </div>
+        </div>
+        </div>
+    </section>
+   );
+}
+
+export default ChunsikList;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
+import '../public/assets/css/style.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+
+import Header from '../components/layouts/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+
+  const isLogin = true;
+  const router = useRouter();
+  console.log(router.pathname);
+
+  return (
+    <div className='container'>
+      {
+        router.pathname === '/login' ? null :  <Header />
+      }
+      <Component {...pageProps} />
+    </div>
+    ); 
+  }
