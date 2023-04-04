4204dd9d3fa6a27a73b3bd641c6a8bce8214682b
swj9707 <swj1265@naver.com>
2023-04-05 01:46:22 +0900

    setup : interceptor  재발급 시 try catch 로 변경 및 테스트

--- Service/ProductService/ProductService.ts
+++ Service/ProductService/ProductService.ts
@@ -3,7 +3,33 @@ import {
   REQUEST_CATEGORY_AGGREGATION_NAME,
   REQUEST_CATEGORY_AGGREGATION_HASH_TAG,
   REQUEST_PRODUCT,
+  REQUEST_RECOMMEND_ACTIVE,
+  REQUEST_EVENT_ACTIVE,
+  REQUEST_BANNER,
+  REQUEST_RECOMMEND_READ,
 } from "@/constants/Apis/URL";
+
+export async function RequestRecommendActive() {
+  return await CustomAxios.get(REQUEST_RECOMMEND_ACTIVE).then(
+    (res) => res.data
+  );
+}
+
+export async function RequestRecommendProduct(recommendId: number) {
+  return await CustomAxios.get(REQUEST_RECOMMEND_READ, {
+    params: {
+      recommendId: recommendId,
+    },
+  }).then((res) => res.data);
+}
+
+export async function RequestEventActive() {
+  return await CustomAxios.get(REQUEST_EVENT_ACTIVE).then((res) => res.data);
+}
+
+export async function RequestBanner() {
+  return await CustomAxios.get(REQUEST_BANNER).then((res) => res.data);
+}
 
 export async function RequestCategoryAggregationName(productName: string) {
   return await CustomAxios.get(REQUEST_CATEGORY_AGGREGATION_NAME, {
